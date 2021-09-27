bc7fd08db41e78f05bfe8fd947943ad5b0fa3165
tysonbattistella <tysonbattistella@gmail.com>
2021-09-27 13:53:35 -0400

    [feature] Browse skeleton code

---
+++ pages/browse.tsx
@@ -0,0 +1,12 @@
+import Root from '../components/Root';
+import { Box, Flex } from '@theme-ui/components';
+
+export default function Browse() {
+  return (
+    <Root>
+      <Flex>
+        <Box></Box>
+      </Flex>
+    </Root>
+  );
+}
