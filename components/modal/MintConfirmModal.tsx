db11e91308cca67ffaa1e4c5275f17e1e9fbfead
tysonbattistella <tysonbattistella@gmail.com>
2021-09-27 22:51:53 -0400

    [fix] Clean up mint button

--- components/modal/MintConfirmModal.tsx
+++ components/modal/MintConfirmModal.tsx
@@ -3,14 +3,16 @@ import { useMint } from '../../hooks/useMint';
 import { Box } from '@theme-ui/components';
 import theme from '../../styles/theme';
 import { DisarrayPreview } from '../DisarrayPreview';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useWeb3 } from '../../hooks/useWeb3';
 import { Disarray, Disarray__factory } from '../../typechain';
 import { useRouter } from 'next/router';
+import { useModal } from '../../hooks/useModal';
 
 enum MintingState {
   NOT_READY = 'NOT_READY',
   READY = 'READY',
+  WAITING = 'WAITING',
   ERROR = 'ERROR',
   BROADCASTED = 'BROADCASTED',
   CONFIRMED = 'CONFIRMED',
@@ -20,6 +22,7 @@ export default function MintConfirmModal() {
   const { name, prepareMintData, ready } = useMint();
   const { library, account } = useWeb3();
   const router = useRouter();
+  const { closeModal } = useModal();
   const [mintingState, setMintingState] = useState<MintingState>(
     MintingState.NOT_READY
   );
@@ -39,11 +42,17 @@ export default function MintConfirmModal() {
       setMintingState(MintingState.ERROR);
       return;
     }
-    if (mintingState === MintingState.CONFIRMED) {
+    if (mintingState === MintingState.ERROR) {
       setMintingState(MintingState.READY);
-      await router.push('/');
     }
-    if (mintingState !== MintingState.READY) {
+    if (mintingState === MintingState.CONFIRMED) {
+      closeModal();
+      await router.push('/');
+      setMintingState(MintingState.READY);
+    }
+    if (
+      ![MintingState.READY, MintingState.ERROR].includes(mintingState as any)
+    ) {
       return;
     }
     const disarray = Disarray__factory.connect(
@@ -53,6 +62,7 @@ export default function MintConfirmModal() {
     const data = prepareMintData();
 
     try {
+      setMintingState(MintingState.WAITING);
       const tx = await disarray.mint(account, data);
       setMintingState(MintingState.BROADCASTED);
       await tx.wait();
@@ -63,19 +73,22 @@ export default function MintConfirmModal() {
     }
   }, [mintingState]);
 
-  let buttonText;
-  switch (mintingState) {
-    case MintingState.NOT_READY:
-      'PREPARING...';
-    case MintingState.BROADCASTED:
-      buttonText = 'AWAITING CONFIRMATION';
-    case MintingState.CONFIRMED:
-      buttonText = 'CONFIRMED. CLICK TO CLOSE';
-    case MintingState.ERROR:
-      buttonText = 'SOMETHING WENT WRONG, TRY AGAIN';
-    default:
-      buttonText = 'MINT';
-  }
+  const buttonText = useMemo(() => {
+    switch (mintingState) {
+      case MintingState.NOT_READY:
+        return 'PREPARING...';
+      case MintingState.WAITING:
+        return 'CHECK YOUR WALLET TO CONFIRM';
+      case MintingState.BROADCASTED:
+        return 'AWAITING CONFIRMATION';
+      case MintingState.CONFIRMED:
+        return 'CONFIRMED. CLICK TO CLOSE';
+      case MintingState.ERROR:
+        return 'SOMETHING WENT WRONG, TRY AGAIN';
+      default:
+        return 'MINT';
+    }
+  }, [mintingState]);
 
   return (
     <ModalContainer title={'Mint'} canClose={true}>
